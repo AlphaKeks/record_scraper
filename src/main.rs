dff97ee6bde70808018e57666a4a7009eff8156e
AlphaKeks <alphakeks@schnose.eu>
2022-10-21 02:35:07 +0200

    0.2.0: cleaner version

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-use std::{
-	fs::{File, OpenOptions},
-	io::{stdin, ErrorKind, Write},
-	time::Duration,
-};
+use std::io::Write;
 
-use serde::{Deserialize, Serialize};
+enum UserInput {
+	ID(u32),
+	Path(String),
+}
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
 struct Record {
 	pub id: Option<i32>,
 	pub steamid64: Option<String>,
@@ -31,110 +30,118 @@ struct Record {
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 4)]
 async fn main() {
-	// get starting id
-	println!("ID to start with:");
-	let mut starting_id = String::new();
+	let (start_fw, start_bw) = (
+		get_input("Please input an ID to start at:", true),
+		get_input("Please input an ID to go backwards from:", true),
+	);
 
-	stdin()
-		.read_line(&mut starting_id)
-		.expect("Failed to read line. PogO");
+	if let UserInput::Path(output_file) = get_input("/path/to/output.txt", false) {
+		let file1 = output_file.clone();
+		let file2 = output_file;
 
-	let starting_id = starting_id
-		.trim()
-		.parse::<u128>()
-		.expect("Please input a valid ID.");
+		let handles = [
+			// spawn thread to fetch new records
+			tokio::spawn(async move {
+				let delay = std::time::Duration::from_millis(727);
+				let sleep_duration = std::time::Duration::from_secs(60 * 5);
 
-	println!("ID to resume at:");
-	let mut resume_id = String::new();
+				if let UserInput::ID(start_fw) = start_fw {
+					fetch_records(file1, start_fw.., delay, sleep_duration).await;
+				}
+			}),
+			// spawn thread to go backwards and fetch older records
+			tokio::spawn(async move {
+				let delay = std::time::Duration::from_millis(727);
+				let sleep_duration = std::time::Duration::from_secs(60 * 5);
 
-	stdin()
-		.read_line(&mut resume_id)
-		.expect("Failed to read line. PogO");
+				if let UserInput::ID(start_bw) = start_bw {
+					fetch_records(file2, (0..start_bw).rev(), delay, sleep_duration).await;
+				}
+			}),
+		];
 
-	let resume_id = resume_id
-		.trim()
-		.parse::<u128>()
-		.expect("Please input a valid ID.");
-
-	// get output file
-	println!("path/to/output_file.txt:");
-	let mut output_file = String::new();
-
-	stdin()
-		.read_line(&mut output_file)
-		.expect("Failed to read line. PogO");
-
-	let output_file1 = output_file.trim().to_owned();
-	let output_file2 = output_file1.clone();
-
-	let handles = [
-		// continuously fetching new records
-		tokio::spawn(async move {
-			let client = reqwest::Client::new();
-			let output_file = output_file1;
-
-			let mut file = match OpenOptions::new()
-				.write(true)
-				.append(true)
-				.open(&output_file)
-			{
-				Ok(file) => file,
-				Err(why) => match why.kind() {
-					ErrorKind::NotFound => match File::create(output_file) {
-						Ok(file) => file,
-						Err(why) => panic!("Failed to create file: {:#?}", why),
-					},
-					_ => panic!("Failed to open file: {:#?}", why),
-				},
-			};
-
-			for i in starting_id.. {
-				match get_record(i, &client).await {
-					Err(_) => {
-						println!("No new records, going to sleep for 5 minutes.");
-						tokio::time::sleep(Duration::from_secs(60 * 5)).await;
-						continue;
-					}
-					Ok(record) => write_to_file(&mut file, &record, i),
-				};
-			}
-		}),
-		// going back in time to get older records
-		tokio::spawn(async move {
-			let client = reqwest::Client::new();
-			let output_file = output_file2;
-
-			let mut file = match OpenOptions::new()
-				.write(true)
-				.append(true)
-				.open(&output_file)
-			{
-				Ok(file) => file,
-				Err(why) => match why.kind() {
-					ErrorKind::NotFound => match File::create(output_file) {
-						Ok(file) => file,
-						Err(why) => panic!("Failed to create file: {:#?}", why),
-					},
-					_ => panic!("Failed to open file: {:#?}", why),
-				},
-			};
-
-			for i in (0..resume_id).rev() {
-				match get_record(i, &client).await {
-					Err(why) => println!("Failed to get record #{}:\n{:#?}", i, why),
-					Ok(record) => write_to_file(&mut file, &record, i),
-				};
-				tokio::time::sleep(Duration::from_millis(727)).await;
-			}
-		}),
-	];
-
-	for handle in handles {
-		let _ = handle.await;
+		for handle in handles {
+			let _ = handle.await;
+		}
 	}
 }
 
-async fn get_record(id: u128, client: &reqwest::Client) -> Result<Record, String> {
+fn get_input<'a>(msg: &'a str, parse: bool) -> UserInput {
+	println!("{}", msg);
+	let mut input = String::new();
+	std::io::stdin()
+		.read_line(&mut input)
+		.expect("Failed to read line. PogO");
+	let input = input.trim();
+
+	if parse {
+		UserInput::ID(input.parse::<u32>().expect("Please input a valid ID."))
+	} else {
+		UserInput::Path(input.to_owned())
+	}
+}
+
+fn write_to_file(file: &mut std::fs::File, entry: Record, idx: &u32) {
+	let json = match serde_json::to_string(&entry) {
+		Ok(json) => json,
+		Err(why) => {
+			println!("Failed to deserialize record #{idx}\n{:#?}", why);
+			return;
+		}
+	};
+
+	match writeln!(file, "{}", json) {
+		Ok(_) => println!("wrote record #{idx} to disk."),
+		Err(why) => println!("failed to write record #{idx} to disk.\n{:#?}", why),
+	}
+}
+
+async fn fetch_records<T>(
+	output_file: String,
+	range: T,
+	delay: std::time::Duration,          // delay between each request
+	sleep_duration: std::time::Duration, // time to wait if fetching a record was unsuccessful
+) where
+	T: Iterator<Item = u32>,
+{
+	let client = reqwest::Client::new();
+	let mut file = match std::fs::OpenOptions::new()
+		.write(true)
+		.append(true)
+		.open(&output_file) // try to open file as writable
+	{
+		Ok(file) => file,
+		Err(why) => match why.kind() {
+			// create file if it doesn't exist
+			std::io::ErrorKind::NotFound => match std::fs::File::create(output_file) {
+				Ok(file) => file,
+				Err(why) => panic!("Failed to create file: {:#?}", why),
+			},
+			kind => panic!("Failed to open file: {:#?}\n{:#?}", kind, why),
+		},
+	};
+
+	// fetch records for given range
+	for i in range {
+		match get_record(&i, &client).await {
+			Ok(record) => write_to_file(&mut file, record, &i),
+			Err(_) => {
+				println!(
+					"no records found; sleeping for {} seconds.",
+					&sleep_duration.as_secs()
+				);
+				// no records found anymore, sleep for x seconds
+				tokio::time::sleep(sleep_duration).await;
+				continue;
+			}
+		}
+
+		// delay after each request to not get rate limited
+		tokio::time::sleep(delay).await;
+	}
+}
+
+async fn get_record(id: &u32, client: &reqwest::Client) -> Result<Record, String> {
 	match client
 		.get(format!("https://kztimerglobal.com/api/v2/records/{id}"))
 		.send()
@@ -145,19 +152,5 @@ async fn get_record(id: u128, client: &reqwest::Client) -> Result<Record, String
 			Err(why) => Err(why.to_string()),
 			Ok(record) => Ok(record),
 		},
-	}
-}
-
-fn write_to_file(file: &mut File, record: &Record, i: u128) {
-	let json = match serde_json::to_string(record) {
-		Err(why) => {
-			return println!("Failed to deserialize record #{}\n:{:#?}", i, why);
-		}
-		Ok(json) => json,
-	};
-
-	match writeln!(file, "{}", json) {
-		Err(why) => println!("Failed to write to file: {:#?}", why),
-		Ok(_) => println!("got record #{i}"),
 	}
 }
